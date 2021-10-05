ed2670a3d4316a29a87d7297a792c353f64baac9
Joshua Kimbareeba <jkimbareeba@yahoo.com>
2021-10-05 12:12:21 +0300

    Static-Generation

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,19 @@ import Head from 'next/head'
 import Link from 'next/link'
 import Layout, { siteTitle } from "../components/layout";
 import utilStyles from "../styles/utils.module.css";
+import {getSortedPostsData} from '../lib/posts';
 
-export default function Home() {
+export async function getStaticProps(){
+  const allPostsData = getSortedPostsData()
+
+  return {
+    props : {
+      allPostsData
+    }
+  }
+}
+
+export default function Home({allPostsData}) {
   return (
     <Layout home>
       <Head>
@@ -11,11 +22,29 @@ export default function Home() {
       </Head>
 
       <section className={utilStyles.headingMd}>
-        <p>Hi, my name is Joshua Kimbareeba and am in love with sofware development :)</p>
         <p>
-          You can find my personal website -  {" "}
+          Hi, my name is Joshua Kimbareeba and am in love with software
+          development :)
+        </p>
+        <p>
+          You can find all my works on my personal website -{" "}
           <a href="https://joshuakimbareeba.co.ke">my portifolio</a>.
         </p>
+      </section>
+
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
       </section>
     </Layout>
   );
